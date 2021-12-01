71df276c6ef27eb4907f7ceed0b21a0258a6e0fc
Hojjat <hojjat12000@yahoo.com>
2021-12-01 14:11:39 -0700

    added answer to day 1 question 2

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,7 @@
 //
 use std::fs;
 fn main() {
-    //let input = [199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
-    //for i in input.iter() {
-    //    println!("{:?}",i);
-    //}
-    //let mut it = input.iter();
-    //for i in 0..11 {
-    //    println!("{:?}", it.next());
-    //}
+    // day 1, question 1
     let content = fs::read_to_string("input").expect("Couldn't read the input");
     println!("{:?}",content
              .split('\n')
@@ -21,6 +14,20 @@ fn main() {
                       (counter, Some(value)) if value < x => (counter+1, Some(x)), // increase
                       (_, None) => (0, Some(x)), // happens for first item
                       (counter, _) => (counter, Some(x)), // don't increase
+                  }
+                ).0);
+
+    // day1, question 2
+    println!("{:?}",content
+             .split('\n')
+             .into_iter()
+             .filter(|str| !str.parse::<i32>().is_err())
+             .map(|str| str.parse::<i32>().unwrap())
+             .fold((0,None, None, None), |acc, x| 
+                  match acc {
+                      (counter, Some(a), Some(b), Some(c)) if a < x => (counter+1, Some(b), Some(c), Some(x)), // increase
+                      (_, None, b, c) => (0, b, c, Some(x)), // happens for first item
+                      (counter, a, b, c) => (counter, b, c, Some(x)), // don't increase
                   }
                 ).0);
     
