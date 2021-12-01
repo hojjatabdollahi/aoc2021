4677ce182a20ebc59ff6070bbb14686478ad4237
Hojjat <hojjat12000@yahoo.com>
2021-12-01 15:39:51 -0700

    added solution with using windows

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,12 +3,18 @@
 use std::fs;
 fn main() {
     // day 1, question 1
-    let content = fs::read_to_string("input").expect("Couldn't read the input");
-    println!("{:?}",content
-             .split('\n')
-             .into_iter()
-             .filter(|str| !str.parse::<i32>().is_err())
-             .map(|str| str.parse::<i32>().unwrap())
+    let content = fs::read_to_string("input")
+        .expect("Couldn't read the input")
+        .lines()
+        .map(|line| line.parse().unwrap())
+        .collect::<Vec<i32>>();
+
+    println!("P1: Using windows and folding {:?}",content
+             .windows(2)
+             .fold(0,|acc, w| acc + (w[1]>w[0]) as i32 ));
+
+    println!("P1: Uisng just folding {:?}",content
+             .iter()
              .fold((0,None), |acc, x| 
                   match acc {
                       (counter, Some(value)) if value < x => (counter+1, Some(x)), // increase
@@ -17,12 +23,14 @@ fn main() {
                   }
                 ).0);
 
+
     // day1, question 2
-    println!("{:?}",content
-             .split('\n')
-             .into_iter()
-             .filter(|str| !str.parse::<i32>().is_err())
-             .map(|str| str.parse::<i32>().unwrap())
+    println!("P2: Using windows and folding {:?}",content
+             .windows(4)
+             .fold(0,|acc, w| acc + (w[3]>w[0]) as i32 )); // you only need to compare the 4th and the first
+
+    println!("P2: Using just folding {:?}",content
+             .iter()
              .fold((0,None, None, None), |acc, x| 
                   match acc {
                       (counter, Some(a), Some(b), Some(c)) if a < x => (counter+1, Some(b), Some(c), Some(x)), // increase
