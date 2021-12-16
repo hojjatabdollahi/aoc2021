9e9454cdda7c4271b855bbe698b3dc2f060419bd
Hojjat <hojjat.abdollahi@gmail.com>
2021-12-15 19:33:57 -0700

    Horrible solution to part 1 and 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,32 +1,30 @@
 type ID = usize;
-#[derive(Clone, Debug)]
+
+#[derive(Clone, Debug, PartialEq)]
 enum CaveType {
     Small,
     Big,
 }
-#[derive(Clone, Debug)]
-enum Status {
-    Seen,
-    NotSeen,
-}
+
+
 #[derive(Debug)]
 struct Caves {
-    caves: Vec<(String, CaveType, Status)>,
+    caves: Vec<(String, CaveType)>,
 }
+
 impl Caves {
     fn find(&self, input: &str) -> Option<ID> {
         self.caves.iter().position(|c| c.0.as_str() == input)
     }
 
-    fn copy(&self, id: ID) -> (String, CaveType, Status) {
+    fn copy(&self, id: ID) -> (String, CaveType) {
         (
             self.caves[id].0.clone(),
             self.caves[id].1.to_owned(),
-            self.caves[id].2.to_owned(),
         )
     }
 
-    fn get(&self, id: ID) -> Option<(String, CaveType, Status)> {
+    fn get(&self, id: ID) -> Option<(String, CaveType)> {
         if id < self.caves.len() {
             Some(self.copy(id))
         } else {
@@ -44,7 +42,6 @@ impl Caves {
                     } else {
                         CaveType::Small
                     },
-                    Status::NotSeen,
                 ));
                 self.caves.len() - 1
             }
@@ -61,19 +58,81 @@ impl Caves {
         self.caves.len()
     }
 }
+struct Connections {
+    data: Vec<(ID, Vec<ID>)>,
+}
+
+impl Clone for Connections {
+    fn clone(&self) -> Self {
+        let mut res: Vec<(ID, Vec<ID>)> = Vec::new();
+        for (x, y) in &self.data {
+            res.push((x.clone(), y.iter().cloned().collect::<Vec<ID>>()));
+        }
+        Self { data: res }
+    }
+}
+
 #[derive(Debug)]
 struct CavesConnection {
     caves: Caves,
     connections: Vec<(ID, Vec<ID>)>,
-    answers: Vec<Vec<usize>>,
 }
 impl CavesConnection {
     fn new() -> Self {
         Self {
             caves: Caves::new(),
             connections: Vec::new(),
-            answers: Vec::new(),
         }
+    }
+
+    fn traverse(
+        &self,
+        current_id: usize,
+        current_path: Vec<usize>,
+        double_possible: bool,
+        used_double: bool,
+    ) -> Vec<Vec<usize>> {
+        let mut res = Vec::new();
+        let to_test = self
+            .connections
+            .clone()
+            .into_iter()
+            .filter(|c| c.0 == current_id)
+            .map(|c| c.1)
+            .collect::<Vec<_>>()[0]
+            .clone();
+
+        let end_id = &self.caves.find("end").unwrap();
+        let start_id = &self.caves.find("start").unwrap();
+        for id in to_test {
+            if &id == end_id {
+                let mut path = current_path.clone();
+                path.push(id);
+                res.push(path);
+            } else {
+                if !current_path.contains(&id) || self.caves.get(id).unwrap().1 == CaveType::Big {
+                    let mut current_path_updated = current_path.clone();
+                    current_path_updated.push(id);
+                    let tmp = self.traverse(id, current_path_updated, double_possible, used_double);
+                    for path in tmp {
+                        res.push(path);
+                    }
+                } else if double_possible && !used_double {
+                    if current_path.iter().filter(|p| *p == &id).count() == 1
+                        && id != *end_id
+                        && id != *start_id
+                    {
+                        let mut current_path_updated = current_path.clone();
+                        current_path_updated.push(id);
+                        let tmp = self.traverse(id, current_path_updated, double_possible, true);
+                        for path in tmp {
+                            res.push(path);
+                        }
+                    }
+                }
+            }
+        }
+        res
     }
 
     fn push(&mut self, input: &str) {
@@ -107,36 +166,36 @@ impl CavesConnection {
         self.connections.len()
     }
 }
+
 fn process1(input: &str) -> usize {
     let mut con = CavesConnection::new();
     for line in input.lines() {
         con.push(line);
     }
-    // let answers = Vec::new();
     println!("{:?}", con.caves);
     let start_id = con.caves.find("start").unwrap();
-    let mut to_test = con
-        .connections
-        .into_iter()
-        .filter(|c| c.0 == start_id)
-        .map(|c| c.1)
-        .collect::<Vec<_>>();
-    loop {
-        match to_test.pop() {
-            Some(x) => {}
-            None => {}
-        }
+    let output = con.traverse(start_id, vec![start_id], false, false);
+    output.len()
+}
 
-        if to_test.len() == 0 {
-            break;
-        }
+fn process2(input: &str) -> usize {
+    let mut con = CavesConnection::new();
+    for line in input.lines() {
+        con.push(line);
     }
-    0
+    println!("{:?}", con.caves);
+    let start_id = con.caves.find("start").unwrap();
+    let output = con.traverse(start_id, vec![start_id], true, false);
+    output.len()
 }
 fn main() {
     println!(
         "Part 1: {}",
         process1(&std::fs::read_to_string("input").expect("Couldn't read the input"))
+    );
+    println!(
+        "Part 2: {}",
+        process2(&std::fs::read_to_string("input").expect("Couldn't read the input"))
     );
 }
 
@@ -188,5 +247,44 @@ b-d
 A-end
 b-end"#;
         assert_eq!(10, process1(input));
+    }
+    #[test]
+    fn part1_2() {
+        let input = r#"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc"#;
+        assert_eq!(19, process1(input));
+    }
+    #[test]
+    fn part2_1() {
+        let input = r#"start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end"#;
+        assert_eq!(36, process2(input));
+    }
+    #[test]
+    fn part2_2() {
+        let input = r#"dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc"#;
+        assert_eq!(103, process2(input));
     }
 }
